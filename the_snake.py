371a7fd6ff5aa260472eee3c5be89f9c8646ca90
Vladimir Smirnov <smirnov.94@mail.ru>
2026-05-06 11:19:44 +0300

    SNAKE_OLD2

--- the_snake.py
+++ the_snake.py
@@ -1,20 +1,20 @@
 import random
-import pygame as pg
 import sys
+import pygame as pg
 
-# Константы для размеров поля и сетки:
+# Константы для размеров поля и сетки
 SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
 GRID_SIZE = 20
 GRID_WIDTH = SCREEN_WIDTH // GRID_SIZE
 GRID_HEIGHT = SCREEN_HEIGHT // GRID_SIZE
 
-# Направления движения:
+# Направления движения
 UP = (0, -1)
 DOWN = (0, 1)
 LEFT = (-1, 0)
 RIGHT = (1, 0)
 
-# Цвета:
+# Цвета
 BOARD_BACKGROUND_COLOR = (0, 0, 0)
 BORDER_COLOR = (93, 216, 228)
 APPLE_COLOR = (255, 0, 0)
@@ -36,11 +36,12 @@ if not _is_testing:
     pg.font.init()
     screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pg.display.set_caption(
-        'Змейка | Стрелки - движение | ESC - выход | +/- скорость')
+        'Змейка | Стрелки - движение | ESC - выход | +/- скорость'
+    )
     clock = pg.time.Clock()
     font = pg.font.Font(None, 36)
 else:
-    # Для тестов создаём заглушки
+    # Заглушки для тестов
     screen = pg.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pg.time.Clock()
     font = pg.font.Font(None, 36)
@@ -54,23 +55,38 @@ class GameObject:
         self.body_color = body_color
 
     def draw_cell(self, position, color, draw_border=True):
+        """
+        Рисует ячейку на экране по позиции и выбранному цвету.
+        :param position: координаты ячейки
+        :param color: цвет ячейки
+        :param draw_border: рисовать ли рамку
+        """
         rect = pg.Rect(position, (GRID_SIZE, GRID_SIZE))
         pg.draw.rect(screen, color, rect)
         if draw_border:
             pg.draw.rect(screen, BORDER_COLOR, rect, 1)
 
     def draw(self):
+        """
+        Метод, который должен быть реализован в дочерних классах.
+        """
         raise NotImplementedError(
             'Метод draw() должен быть переопределен в дочернем классе'
         )
 
 
 class Apple(GameObject):
+    """Класс для яблока."""
+
     def __init__(self, used_positions=None):
         super().__init__(body_color=APPLE_COLOR)
         self.randomize_position(used_positions or [])
 
     def randomize_position(self, used_positions=None):
+        """
+        Располагает яблоко в случайной позиции, которая не занята.
+        :param used_positions: занятые позиции игрока
+        """
         while True:
             self.position = (
                 random.randrange(GRID_WIDTH) * GRID_SIZE,
@@ -80,10 +96,13 @@ class Apple(GameObject):
                 break
 
     def draw(self):
+        """Отрисовка яблока."""
         self.draw_cell(self.position, self.body_color)
 
 
 class Snake(GameObject):
+    """Класс для змейки."""
+
     def __init__(self):
         super().__init__(body_color=SNAKE_COLOR)
         self.length = 1
@@ -92,9 +111,16 @@ class Snake(GameObject):
         self.last = None
 
     def update_direction(self, new_direction):
+        """
+        Обновляет направление движения змейки.
+        :param new_direction: новое направление
+        """
         self.direction = new_direction
 
     def move(self):
+        """
+        Перемещает змейку в текущем направлении.
+        """
         head_x, head_y = self.get_head_position()
         dir_x, dir_y = self.direction
         new_head = (
@@ -108,13 +134,16 @@ class Snake(GameObject):
             self.last = None
 
     def draw(self):
+        """Отрисовка змейки."""
         for position in self.positions:
             self.draw_cell(position, self.body_color)
 
     def get_head_position(self):
+        """Возвращает позицию головы змейки."""
         return self.positions[0]
 
     def reset(self):
+        """Сброс состояния змейки при поражении."""
         self.position = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
         self.length = 1
         self.positions = [self.position]
@@ -123,6 +152,11 @@ class Snake(GameObject):
 
 
 def handle_keys(game_object):
+    """
+    Обработка нажатий клавиш.
+    :param game_object: текущий объект (змейка)
+    :return: False при закрытии окна или ESC
+    """
     global SPEED
     for event in pg.event.get():
         if event.type == pg.QUIT:
@@ -133,7 +167,7 @@ def handle_keys(game_object):
                 return False
 
             # Управление скоростью
-            if event.key == pg.K_PLUS or event.key == pg.K_EQUALS:
+            if event.key in (pg.K_PLUS, pg.K_EQUALS):
                 SPEED = min(SPEED + SPEED_STEP, MAX_SPEED)
             elif event.key == pg.K_MINUS:
                 SPEED = max(SPEED - SPEED_STEP, MIN_SPEED)
@@ -152,11 +186,13 @@ def handle_keys(game_object):
 
 
 def draw_speed():
+    """Отображение текущей скорости."""
     speed_text = font.render(f'Скорость: {SPEED}', True, TEXT_COLOR)
     screen.blit(speed_text, (10, 10))
 
 
 def main():
+    """Основная функция запуска игры."""
     snake = Snake()
     apple = Apple(snake.positions)
 
@@ -178,6 +214,7 @@ def main():
 
         snake.move()
 
+        # Проверка на столкновение с собой
         if snake.get_head_position() in snake.positions[1:]:
             snake.reset()
             apple.randomize_position(snake.positions)
